5ffa6ce7ca475f9d2425ad9970d7871d4dedfa42
Sourov <sourovdas730@gmail.com>
2024-05-17 17:05:32 +0600

    commit for initial deploy to vercel

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const database = client.db("cravio");
         const foodCollection = database.collection("foods");
 
@@ -40,12 +40,12 @@ async function run() {
 
         app.post('/add-food', async(req, res) => {
             const foodDetails = req.body;
-            console.log(foodDetails);
+            // console.log(foodDetails);
             const result = await foodCollection.insertOne(foodDetails);
             res.send(result);
         })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error

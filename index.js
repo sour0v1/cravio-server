73cd9d2f963b05295c583785207759ea4b5fb681
Sourov <sourovdas730@gmail.com>
2024-06-02 08:20:25 +0600

    secure api by jwt token

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
-
+app.use(cookieParser());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhnq0hd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,6 +27,34 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+// step 5
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('tokennn -',token);
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized' })
+        }
+        // console.log('userrr - ', decoded)
+        req.user = decoded;
+        next();
+    })
+
+}
+// step 6
+const verifyUser = (req, res, next) => {
+    const userEmail = req.query.email;
+    if (userEmail !== req.user.email) {
+        return res.status(401).send({ message: 'unauthorized' })
+    }
+    next();
+}
+// finally use middleware in the api
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -30,6 +62,36 @@ async function run() {
         const database = client.db("cravio");
         const foodCollection = database.collection("foods");
         const requestFoodCollection = database.collection('requestedFood');
+
+        // api to set token to the cookie
+        // [step 2, 4 from client side]
+        // step 1
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'None'
+            })
+            res.send({ success: true });
+        })
+        // api to remove token from cookie
+        // step 3
+        app.post('/remove-token', async (req, res) => {
+            const user = req.body;
+            // console.log('user - ', user)
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: true,
+                sameSite: true,
+                maxAge: 0
+            })
+            res.send({ success: true })
+        })
         //---ok
         app.get('/available-foods', async (req, res) => {
             const { availability } = req.query;
@@ -49,7 +111,7 @@ async function run() {
             res.send(result);
         })
         // ---ok
-        app.get('/manage-food', async (req, res) => {
+        app.get('/manage-food', verifyToken, verifyUser, async (req, res) => {
             const userEmail = req.query.email;
             const query = { donatorEmail: userEmail }
             // console.log(userEmail)
@@ -57,27 +119,29 @@ async function run() {
             res.send(result);
         })
         // ---ok
-        app.put('/food/update', async(req, res) =>{
+        app.put('/food/update', verifyToken, async (req, res) => {
             const food = req.body;
             console.log(food)
             const filter = { _id: new ObjectId(`${food.id}`) }
             const updatedFood = {
-                $set : {
-                    foodName : food.fname,
-                    foodImg : food.fImage,
-                    foodQuantity : food.fQuantity,
-                    date : food.eDate,
-                    notes : food.note,
-                    location : food.pLocation
+                $set: {
+                    foodName: food.fname,
+                    foodImg: food.fImage,
+                    foodQuantity: food.fQuantity,
+                    date: food.eDate,
+                    notes: food.note,
+                    location: food.pLocation
                 }
             }
-            const result = await foodCollection.updateOne(filter,updatedFood);
+            const result = await foodCollection.updateOne(filter, updatedFood);
             res.send(result);
             console.log(updatedFood);
         })
         //---ok
-        app.get('/requested-food', async (req, res) => {
+        app.get('/requested-food', verifyToken, verifyUser, async (req, res) => {
             const userEmail = req.query.email;
+            // console.log('user for the valid token - ', req.user.email)
+            // console.log(userEmail)
             const query = { email: userEmail }
             // console.log(userEmail)
             const result = await requestFoodCollection.find(query).toArray();
@@ -85,20 +149,20 @@ async function run() {
         })
 
         // ---ok
-        app.post('/request-food', async (req, res) => {
+        app.post('/request-food', verifyToken, async (req, res) => {
             const foodDetails = req.body;
             const result = await requestFoodCollection.insertOne(foodDetails);
             res.send(result);
         })
         //---ok
-        app.post('/add-food', async (req, res) => {
+        app.post('/add-food', verifyToken, async (req, res) => {
             const foodDetails = req.body;
             // console.log(foodDetails);
             const result = await foodCollection.insertOne(foodDetails);
             res.send(result);
         })
         // ---ok
-        app.delete('/delete-food/:id', async (req, res) => {
+        app.delete('/delete-food/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             // console.log('deleted',id);
             const query = { _id: new ObjectId(id) }
